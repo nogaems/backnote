32b82dceb64837f45cf4bf1f945b0ec1b14c881a
nogaems <nogaems@users.noreply.github.com>
2017-07-08 00:12:13 +0300

    Add the usage of the functions from src/config.*

--- src/backnote.c
+++ src/backnote.c
@@ -1,8 +1,25 @@
 #include <stdio.h>
+#include <string.h>
 #include "config.h"
 
-int
-main(int argc, char * argv[])
-{    
+struct bn_config conf = {0};
+
+int main(int argc, char * argv[])
+{
+    config_t cfg;
+    char *name;
+    name = get_config_name();   
+   
+    if(!strcmp(name, ""))
+    {
+        fprintf(stderr, "Configuration file not found\n");
+        return 1;
+    } 
+    if (load_config(name, &cfg, &conf))
+    {
+        config_destroy(&cfg);
+        return 1;
+    }
+    print_config(name);
     return 0;
 }
