96220917ee909a40759fc9d501a029651b57462b
nogaems <nogaems@users.noreply.github.com>
2017-07-07 22:39:46 +0300

    Implementation of configuration related functions

--- src/config.c
+++ src/config.c
@@ -1,8 +1,7 @@
+#include <stdio.h>
 #include <unistd.h>
+#include <string.h>
 #include "config.h"
-
-config_t cfg;
-
 
 bool
 check_config_exists(const char * name)
@@ -10,5 +9,84 @@ check_config_exists(const char * name)
     return access(name, R_OK) != -1 ? true : false;
 }
 
+int
+load_config(char *name, config_t *cfg, struct bn_config *conf)
+/* 
+   cfg is a pointer to libconfig structure
+   conf is a pointer to the internal configuration structure
+*/
+{
+    config_setting_t *setting;    
+    config_init(cfg);    
+    if(!config_read_file(cfg, name))
+    {
+        fprintf(stderr, "Can't read configuration file \"%s\"\n", name);
+        return -1;
+    }
+    
+    if(!config_lookup_string(cfg, "wallpaper", &conf->wallpaper))
+    {
+        fprintf(stderr, "Configuration error. Please, set \"wallpaper\" parameter.\n");
+        return -1;
+    }
+    if(!config_lookup_bool(cfg, "daemon", &conf->daemon))
+    {
+        fprintf(stderr, "Configuration error. Please, set \"daemon\" parameter.\n");
+        return -1; 
+    }
+    
+    if (!(setting = config_lookup(cfg, "notes")))
+    {
+        fprintf(stderr, "Section \"notes\" is missing.\n");  
+        return -1;
+    }
+    
+    conf->notes_count = config_setting_length(setting);
+    for (int i = 0; i < conf->notes_count; i++)
+    {        
+        config_setting_t *note = config_setting_get_elem(setting, i);
+        LOAD_STR(note, "file", &conf->notes[i].file, 1);
+        
+        config_setting_t *position = config_setting_lookup(note, "position");
+        LOAD_INT(position, "x1", &conf->notes[i].position.x1, 1);
+        LOAD_INT(position, "y1", &conf->notes[i].position.y1, 1);
+        LOAD_INT(position, "x2", &conf->notes[i].position.x2, 0);
+        LOAD_INT(position, "y2", &conf->notes[i].position.y2, 0);
+        LOAD_BOOL(position, "fit", &conf->notes[i].position.fit, 0);
 
+        config_setting_t *style = config_setting_lookup(note, "style");
+        LOAD_STR(style, "font", &conf->notes[i].style.font, 1);
+        LOAD_INT(style, "size", &conf->notes[i].style.size, 1);
+        LOAD_INT(style, "color", &conf->notes[i].style.color, 0);
+        LOAD_BOOL(style, "border", &conf->notes[i].style.border, 0);
+        LOAD_INT(style, "border_color", &conf->notes[i].style.border_color, 0);
+        LOAD_INT(style, "border_thickness", &conf->notes[i].style.border_thickness, 0);
+        LOAD_INT(style, "bg_color", &conf->notes[i].style.bg_color, 0);        
+    }
+    return 0;
+}
 
+void
+print_config(char *name)
+{
+    printf("wallpaper: %s\n", conf.wallpaper);
+    printf("daemon: %i\n", conf.daemon);   
+    printf("----------\nnotes count: %i\n", conf.notes_count);
+    for (int i = 0; i < conf.notes_count; i++)
+    {
+        printf("note #%i:\n", i);
+        printf("note file: %s\n", conf.notes[0].file);
+        printf("x1: %i\n", conf.notes[0].position.x1);
+        printf("y1: %i\n", conf.notes[0].position.y1);
+        printf("x2: %i\n", conf.notes[0].position.x2);
+        printf("y2: %i\n", conf.notes[0].position.y2);
+        printf("fit: %i\n", conf.notes[0].position.fit);
+        printf("font: %s\n", conf.notes[0].style.font);
+        printf("size: %i\n", conf.notes[0].style.size);
+        printf("color: %i\n", conf.notes[0].style.color);
+        printf("border: %i\n", conf.notes[0].style.border);
+        printf("border_color: %i\n", conf.notes[0].style.border_color);
+        printf("border_thickness: %i\n", conf.notes[0].style.border_thickness);
+        printf("bg_color: %i\n", conf.notes[0].style.bg_color);
+    }
+}
--- src/config.h
+++ src/config.h
@@ -2,8 +2,66 @@
 #include <stdbool.h>
 #include <libconfig.h>
 
-extern config_t cfg;
+#define MAX_NOTES 8
 
-bool check_config_exists(const char * name);
-config_t load_config(char * name);
-bool validate_config(config_t config);
+extern struct bn_config conf;
+
+/* setting - config_t *, 
+name - char *, 
+destination - pointer, 
+required - 1 or 0 */
+#define LOAD_INT(s, n, d, r) \
+    if(!config_setting_lookup_int(s, n, d) && r)  \
+    { \
+        fprintf(stderr, "Configuration error. Please, set \"%s\" parameter.\n", n); \
+        return -1; \
+    }
+#define LOAD_STR(s, n, d, r) \
+    if(!config_setting_lookup_string(s, n, d) && r) \
+    { \
+        fprintf(stderr, "Configuration error. Please, set \"%s\" parameter.\n", n); \
+        return -1; \
+    }
+#define LOAD_BOOL(s, n, d, r) \
+    if(!config_setting_lookup_bool(s, n, d) && r)  \
+    { \
+        fprintf(stderr, "Configuration error. Please, set \"%s\" parameter.\n", n); \
+        return -1; \
+    }
+
+/* Note style */
+
+struct bn_note_style {
+    const char *font;
+    int size;
+    int color;
+    int border;
+    int border_thickness;
+    int border_color;
+    int bg_color;
+};
+
+/* Note position */
+struct bn_note_position {
+    int x1, x2, y1, y2;
+    int fit;
+};
+
+/* Note structure */
+struct bn_note {
+    const char *file;
+    struct bn_note_position position;
+    struct bn_note_style style;
+};
+
+/* Processed config */
+struct bn_config {
+    const char *wallpaper;
+    int daemon;
+    struct bn_note notes[MAX_NOTES];
+    unsigned int notes_count;
+};
+
+bool check_config_exists(const char *name);
+int load_config(char *name, config_t *cfg, struct bn_config *conf);
+void print_config(char *name);
