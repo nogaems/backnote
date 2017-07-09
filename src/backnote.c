077e40d2a15cedac0968e4cb930feb508c5863f0
nogaems <nogaems@users.noreply.github.com>
2017-07-09 13:38:02 +0300

    Split main entry point code into two functions init function will do all configuration and preparations of the program, clean_up will do free the memory that has been allocated.

--- src/backnote.c
+++ src/backnote.c
@@ -2,28 +2,52 @@
 #include <string.h>
 #include <stdlib.h>
 #include "config.h"
+#include "render.h"
 
 struct bn_config conf = {0};
+config_t cfg;
+char *config_name;
+
+bool
+init()
+{
+    config_name = get_config_name();
+   
+    if(!strcmp(config_name, ""))
+    {
+        fprintf(stderr, "Configuration file not found\n");
+        return false;
+    } 
+    if (load_config(config_name, &cfg, &conf))
+    {
+        config_destroy(&cfg);
+        return false;
+    }
+    if(!validate_config(&conf))
+    {
+        fprintf(stderr, "Configuration file is not valid\nExiting\n");
+        return false;
+    }
+    for(int i = 0; i < conf.notes_count; i++)
+    {
+        load_note_text(&conf.notes[i]);
+    }
+    return true;
+}
+
+void
+clean_up()
+{
+    free(config_name);
+    for(int i = i; i < conf.notes_count; i++)
+    {
+        free(conf.notes[i].text);
+    }
+}
 
 int main(int argc, char * argv[])
 {
-    config_t cfg;
-    char *name;
-    name = get_config_name();   
-   
-    if(!strcmp(name, ""))
-    {
-        fprintf(stderr, "Configuration file not found\n");
+    if(!init())
         return 1;
-    } 
-    if (load_config(name, &cfg, &conf))
-    {
-        config_destroy(&cfg);
-        return 1;
-    }
-    print_config(name);
-    if(!validate_config(&conf))
-        fprintf(stderr, "Configuration file is not valid\nExiting\n");
-    free(name);
     return 0;
 }
