07fddb73a410e5211df09e00b979d25de0290f4a
nogaems <nogaems@users.noreply.github.com>
2017-07-13 02:46:51 +0300

    Add the use of prepare_note function

--- src/backnote.c
+++ src/backnote.c
@@ -4,9 +4,11 @@
 #include "config.h"
 #include "render.h"
 
+
 struct bn_config conf = {0};
 config_t cfg;
 char *config_name;
+struct bn_render render = {0};
 
 bool
 init()
@@ -28,9 +30,12 @@ init()
         fprintf(stderr, "Configuration file is not valid\nExiting\n");
         return false;
     }
+    if(!init_render(&render))
+        return false;
     for(int i = 0; i < conf.notes_count; i++)
     {
         load_note_text(&conf.notes[i]);
+        prepare_note(&render, &conf.notes[i]);
     }
     return true;
 }
@@ -43,11 +48,14 @@ clean_up()
     {
         free(conf.notes[i].text);
     }
+    clean_render(&render);
 }
 
 int main(int argc, char * argv[])
 {
     if(!init())
         return 1;
+
+    clean_up();
     return 0;
 }
