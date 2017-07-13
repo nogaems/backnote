75f563ab0db2d19fe9f077791eab14af2f97a6d4
nogaems <nogaems@users.noreply.github.com>
2017-07-13 18:27:29 +0300

    Fix prepare_note function Now it will split wide words by characters if those cannot be splitted by space bar.

--- src/render.c
+++ src/render.c
@@ -95,6 +95,7 @@ void prepare_note(struct bn_render *render, struct bn_note *note)
         {
             note->lbi[note->lbi_counter] = i;
             note->lbi_counter++;
+            space = i;
             memset(buf, 0, max_buf_len);
             continue;
         }
@@ -106,14 +107,16 @@ void prepare_note(struct bn_render *render, struct bn_note *note)
         if(check_fit(render, note, buf))
         {
             continue;
-        } else {
+        } else {            
             if(space == 0 || space == note->lbi[note->lbi_counter-1])
             {
                 /* TODO: Crop this word to the maximum width thus it would fit in the note width */
                 fprintf(stderr, "Warning: word \"%s\" is wider than note area width!\n", buf);
-                continue;
+                note->lbi[note->lbi_counter] = i;
+            } else {
+                note->lbi[note->lbi_counter] = space;
+                i = space;
             }
-            note->lbi[note->lbi_counter] = space;
             note->lbi_counter++;
             memset(buf, 0, max_buf_len);
         }
